b2c0331743205636fb6989d04c9f2733fa89dda6
MJ Johnson <mjohnson4685@gmail.com>
2020-09-10 14:33:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
 def unsafe?(speed)
-
-end
+	if speed != speed
+	    return "HUH?! SPEAK UP, SONNY!"
+	elsif speed == "I LOVE YOU GRANDMA!"
+	    return "I LOVE YOU TOO PUMPKIN!"
+	elsif speed == speed
+	  return "NO, NOT SINCE 1938!"
+	    end
+	end
 
 
 
 def not_safe?(speed)
+
+
+
 	
 end
 	
